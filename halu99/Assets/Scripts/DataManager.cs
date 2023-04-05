c6967607700cbee2382886f728a957591c1fb8b5
halu990716 <halu990716@gmail.com>
2023-04-05 13:49:56 +0900

    halu 1.0

--- halu99/Assets/Scripts/DataManager.cs
+++ halu99/Assets/Scripts/DataManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 using System.Text;
 
@@ -57,7 +58,20 @@ public class DataManager : MonoBehaviour
 
     private void Start()
     {
-        var jsonData = Resources.Load<TextAsset>("saveFile/Data");
+        //var jsonData = Resources.Load<TextAsset>("saveFile/Data");
+
+        // ¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é¡é
+        // public override int Read( Span<byte> buffer );
+        // public override int Read( byte[] array, int offset, int count );
+        ///halu99/UnityBuild
+        byte[] vs = new byte[0];
+
+        FileStream fileStream = new FileStream(
+            Application.dataPath + "/Resources/saveFile/Data.json", FileMode.Open);
+        
+        var jsonData = fileStream.Read(vs, 0, vs.Length);
+
+
         DataForm form = JsonUtility.FromJson<DataForm>(jsonData.ToString());
 
         //bastUserName = "1";
@@ -138,18 +152,55 @@ public class DataManager : MonoBehaviour
         }
     }
 
+    //public void SaveData(string _name, string _clearTime, string _secondName,
+    //    string _secondClearTime, string _thirdName, string _thirdClearTime)
+    //{
+    //    DataForm form = new DataForm(_name, _clearTime, _secondName, _secondClearTime, _thirdName, _thirdClearTime);
+
+    //    string JsonData = JsonUtility.ToJson(form);
+
+    //    FileStream fileStream = new FileStream(
+    //        Application.dataPath + form, FileMode.Create);
+
+    //    byte[] data = Encoding.UTF8.GetBytes(JsonData);
+
+    //    fileStream.Write(data, 0, data.Length);
+    //    fileStream.Close();
+    //}
+
     public void SaveData(string _name, string _clearTime, string _secondName,
-        string _secondClearTime, string _thirdName, string _thirdClearTime)
+       string _secondClearTime, string _thirdName, string _thirdClearTime)
     {
+        GameObject CanvasObject = GameObject.Find("Canvas").gameObject;
+        GameObject TextObject = new GameObject("Test");
+
+        TextObject.transform.SetParent(CanvasObject.transform);
+
+        RectTransform rectTransform = TextObject.AddComponent<RectTransform>();
+
+        rectTransform.anchoredPosition = new Vector2(0.0f, 850.0f);
+        rectTransform.sizeDelta = new Vector2(250.0f, 50.0f);
+
+        Text text = TextObject.AddComponent<Text>();
+
+        text.fontSize = 25;
+        text = GameObject.Find("Test").gameObject.GetComponent<Text>();
+        
+        Font font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+        text.font = font;
+
+        text.text = string.Format( "{0}", Application.dataPath);
+
         DataForm form = new DataForm(_name, _clearTime, _secondName, _secondClearTime, _thirdName, _thirdClearTime);
 
         string JsonData = JsonUtility.ToJson(form);
 
         FileStream fileStream = new FileStream(
-            Application.dataPath + "/Resources/saveFile/Data.json", FileMode.Create);
+            Application.dataPath + "/halu99/UnityBuild/Resources/saveFile/Data.json", FileMode.Create);
 
         byte[] data = Encoding.UTF8.GetBytes(JsonData);
 
+        
         fileStream.Write(data, 0, data.Length);
         fileStream.Close();
     }
