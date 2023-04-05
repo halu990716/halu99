b15ed1c34353e30905c565761dc8e2c10d921a56
halu990716 <halu990716@gmail.com>
2023-04-05 10:04:37 +0900

    halu 1.0

--- halu99/Assets/Scripts/PlayerController.cs
+++ halu99/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Device;
-using static UnityEditor.PlayerSettings;
 
 public class PlayerController : MonoBehaviour
 {
